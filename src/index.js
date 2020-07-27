e6d79a130c32d90d0b9c3c9b8aeecf2ba3ec0953
JimHui12 <jianghua.hui@uq.net.au>
2020-07-27 14:42:06 +1000

    component reuse updated

--- src/index.js
+++ src/index.js
@@ -2,28 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail 
-                author="Sam" 
-                timeAgo="Today at 4:45PM" 
-                content="Hi, how are you"
-                avatar={faker.image.avatar()}
-            />
-            <CommentDetail 
-                author="Alex" 
-                timeAgo="Today at 2:00AM" 
-                content="Hi, how are you"
-                avatar={faker.image.avatar()}
-            />
-            <CommentDetail 
-                author="Jane" 
-                timeAgo="Today at 11:00AM" 
-                content="Hi, how are you"
-                avatar={faker.image.avatar()}
-            />
+            <ApprovalCard>
+                Are you sure?
+            </ApprovalCard>
+            <ApprovalCard >
+                <CommentDetail 
+                    author="Sam" 
+                    timeAgo="Today at 4:45PM" 
+                    content="Hi, how are you"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard >
+                <CommentDetail 
+                    author="Alex" 
+                    timeAgo="Today at 2:00AM" 
+                    content="Hi, how are you"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard >
+                <CommentDetail 
+                    author="Jane" 
+                    timeAgo="Today at 11:00AM" 
+                    content="Hi, how are you"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
         </div>
     );
 };
